b5ea86f6366eafec2e0b42be1a7dccf8d1e5a323
yaozhuojiang <1093331524@qq.com>
2022-10-23 09:42:31 +0800

    feat: add demo22_urdf_gazebo

---
+++ build/demo22_urdf_gazebo/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/boiledman/learn_ros/src/demo22_urdf_gazebo
+# Build directory: /home/boiledman/learn_ros/build/demo22_urdf_gazebo
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
